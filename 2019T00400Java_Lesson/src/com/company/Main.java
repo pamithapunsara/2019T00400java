0f40071ac5fff5fee33e2d90a9495321ad927724
pamithapunsara <pamithapunsara22@gmail.com>
2021-12-28 09:48:21 +0530

    Added if examples to main

--- 2019T00400Java_Lesson/src/com/company/Main.java
+++ 2019T00400Java_Lesson/src/com/company/Main.java
@@ -3,6 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+	// write your code he
+
+        String name="Sri Lanka" + " " + "Country";
+        System.out.println(name);
+
+
+            char vowels= 'n';
+
+            switch (vowels) {
+
+                case 'a':
+                    System.out.println("thia is the vowels");
+                    break;
+                case 'e':
+                    System.out.println("thia is the vowels");
+                    break;
+                case 'i':
+                    System.out.println("thia is the vowels");
+                    break;
+                case 'o':
+                    System.out.println("thia is the vowels");
+                    break;
+                case 'u':
+                    System.out.println("thia thia the voule");
+                    break;
+                default:
+                    System.out.println("constant value");
+            }
+
+        }
+
 }
